c4f00e11d2324398c5ffdb643b30e57dfb9460fc
Saleem3097 <97451705+Saleem3097@users.noreply.github.com>
2022-06-28 10:08:42 +0530

    Update install.sh
    updated tomcat server

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+sudo amazon-linux-extras list
+sudp sudo amazon-linux-extras install tomcat8.5
+sudo systemctl start tomcat
 aws s3 cp s3://codepipeline-ap-south-1-684741742115/sample-demo-java/LoginWebApp-1.war
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart 
