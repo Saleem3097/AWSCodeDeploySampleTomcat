0ca0cbced8a17dbc979edf8035130bf84efa3609
Saleem3097 <97451705+Saleem3097@users.noreply.github.com>
2022-06-28 09:57:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 aws s3 cp s3://java-artifacts-devops4solutions/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
+sudo cp s3://codepipeline-ap-south-1-684741742115/sample-demo-java/LoginWebApp-1.war  /var/www/html/index.html
 sudo service tomcat restart
